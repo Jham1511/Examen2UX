f8ca07c0e036451e38b98ef181a18f7429bec4ab
Víctor Romero <virn2005@gmail.com>
2024-06-18 12:13:22 -0600

    .env added and some added stuff

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const postRoutes = require('./routes/posts');
 app.use('/auth', authRoutes);
 app.use('/posts', postRoutes);
 
-// Connect to MongoDB
+// Connect to MongoDB 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
